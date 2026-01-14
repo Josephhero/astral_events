21c53345f7401ddfd0dffaf0985195048cd9502a
Joseph Hefner <Hefnerjoseph87@gmail.com>
2026-01-14 11:59:15 -0800

    Update monicas_messages.R
    Add code to create the moon phase image

--- R/monicas_messages.R
+++ R/monicas_messages.R
@@ -2,6 +2,9 @@ library(dplyr)
 library(lubridate)
 library(readr)
 library(glue)
+library(ggplot2)
+library(gggibbous)
+library(base64enc)
 
 year <- year(now(tzone = "America/Los_Angeles"))
 astral <- read_csv("./Data/monicas_astral_data.csv")
@@ -23,6 +26,48 @@ next_meteor <- astral |>
 next_eclipse <- astral |>
   filter(full_date > today, type == "eclipse") |>
   slice(1)
+
+# Get tomorrow's illumination to determine waxing vs waning
+tomorrow <- today + 1
+events_tomorrow <- astral |>
+  filter(full_date == tomorrow)
+
+# Determine if waxing (growing) or waning (shrinking)
+is_waxing <- events_tomorrow$illumination[1] > events_today$illumination[1]
+
+# Create moon phase image
+moon_plot <- ggplot() +
+  gggibbous::geom_moon(
+    aes(x = 0, y = 0, ratio = 1, fill = "gray20"),
+    size = 22
+  ) +
+  gggibbous::geom_moon(
+    aes(
+      x = 0,
+      y = 0,
+      ratio = events_today$illumination[1],
+      fill = "#FFFAF0",
+      right = !is_waxing
+    ),
+    size = 20
+  ) +
+  scale_fill_identity() +
+  theme_void() +
+  theme(plot.margin = margin(0, 0, 0, 0))
+
+# Save the plot
+ggsave(
+  "Images/moon_phase.png",
+  moon_plot,
+  width = 1,
+  height = 1,
+  units = "in",
+  dpi = 150,
+  bg = "transparent"
+)
+
+# Convert to base64
+moon_base64 <- base64encode("Images/moon_phase.png")
 
 # Helper function to format date display
 format_date <- function(date, days = NA) {
@@ -55,7 +100,9 @@ em_subject <- glue(
 # Build email content
 message <- glue(
   "<b>{format(events_today$full_date[1], '%b %d, %Y')}</b><br><br>",
-  "<b>Lunar Illumination:</b> {events_today$illumination[1] * 100}%<br>",
+  "<b>Lunar Illumination:</b> {events_today$illumination[1] * 100}%  ",
+  "<img src='data:image/png;base64,{moon_base64}' alt='Moon' style='width:30px;height:30px;vertical-align:middle;'>",
+  "<br>",
   "<b>Age of Moon:</b> {events_today$moon_age[1]} Days<br>",
   "<b>New Moon:</b> {events_today$days_until_new_moon[1]} Days<br><br>",
   "{current_events}<br><br>",
